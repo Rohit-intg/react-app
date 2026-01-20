96e599746e7a45e1868ef4e5419c31a81aecb7d6
Rohit-intg <rohit.kalia@intelegencia.com>
2026-01-20 11:52:30 +0530

    added codes extra

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,32 @@ import React, { useEffect, useState } from "react";
 import ChildBox from "./ChildBox";
 
 function App() {
- 
   const [message, setMessage] = useState("apple");
 
- 
   useEffect(() => {
     console.log("App loaded for the first time");
   }, []);
 
-  
   useEffect(() => {
-    console.log("Message changed to:",message );
+    console.log("Message changed to:", message);
   }, [message]);
 
   return (
     <div style={{ border: "3px solid red", padding: "20px" }}>
       <h2>Parent Div</h2>
 
-      <ChildBox title="Child 1" message={message} />
-      <ChildBox title="Child 2" message={message} />
-      <ChildBox title="Child 3" message={message} />
+      <ChildBox title="Child 1" message={message}>
+        <h2>This is child prop</h2>
+
+      </ChildBox>
+
+      <ChildBox title="Child 2" message={message}>
+        <button>Click Me</button>
+      </ChildBox>
+
+      <ChildBox title="Child 3" message={message}>
+        <strong>Important note inside Child 3</strong>
+      </ChildBox>
 
       <button onClick={() => setMessage("banana")}>
         Change Message
--- src/ChildBox.jsx
+++ src/ChildBox.jsx
@@ -1,9 +1,12 @@
 function ChildBox(props) {
-
+  console.log(props)
   return (
     <div style={{ border: "2px solid blue", margin: "10px", padding: "10px" }}>
       <h3>{props.title}</h3>
       <p>{props.message}</p>
+
+      {/* children will render HERE */}
+      <div>{props.children}</div>
     </div>
   );
 }
